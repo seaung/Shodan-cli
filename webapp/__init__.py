ba3cd618c1b279f1dc197d6269e620299cd20305
seaung <seaung.c0m7de@protonmail.com>
2018-08-21 00:17:24 +0800

    add configruation file and static file.

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object('webapp.settings')
+    app.config.from_object('webapp.secure')
     register_app(app)
     return app
 
