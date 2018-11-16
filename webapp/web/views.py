bb272a48be4869a22fa0466b2717aeeed574a1c9
seaung <seaung.c0m7de@protonmail.com>
2018-11-16 14:10:56 +0800

    test#:(testing code and add search result page.)

--- webapp/web/views.py
+++ webapp/web/views.py
@@ -13,13 +13,28 @@ def index():
 def search():
     form = SearchForm(request.form)
 
-    if request.method == "POST" and form.validate():
-        keyword = form.keyword.data
+    matchers = MatchersCollection()
 
-        return redirect(url_for("host_lists"))
+    if request.method == "POST" and form.validate():
+        keyword = form.keyword.data.strip()
+        
+        ip_or_keyword = is_ip_or_keyword(keyword)
+
+        if ip_or_keyword == 'ip_address':
+            search_collection = SearchCollection()
+            search_collection.search_by_ip(keyword)
+        else:
+            search_collection = SearchCollection()
+            search_collection.search_by_host(keyword)
+        matchers.fill(search_collection, keyword)
+        return render_template("result.html", data=matchers)
+    else:
+        flash("errors.")
+
     return render_template("shodan.html", form=form)
 
 
 @web.route("/hosts", methods=["GET"])
 def host_lists():
     return render_template("result.html")
+
